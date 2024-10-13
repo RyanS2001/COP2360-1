4a3fc5754dcd92cf7cd3b1e45d16cdeb69bd1ddd
Castera36 <medeusc@my.palmbeachstate.edu>
2024-10-13 18:20:58 +0500

    Module 3: Discussion

---
+++ Inheritance.cs
@@ -0,0 +1,17 @@
+﻿// Creating Stock NVDA with 500 shares owned
+Stock nvda = new Stock { Name = "NVDA", SharesOwned = 500 };
+
+Console.WriteLine("Stock: " + nvda.Name);         
+Console.WriteLine("Shares: " + nvda.SharesOwned);  
+
+//Inheritance Concept 
+public class Asset
+{
+    public string Name;
+}
+public class Stock : Asset   // inherits from Asset
+{
+    public long SharesOwned;
+};
+
+
